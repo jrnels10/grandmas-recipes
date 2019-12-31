bc41320693a49b8b764228c4ec183f68ca8aa48a
Jacob <jrnels10@gmail.com>
2019-12-30 17:37:41 -0700

    added prodKeys

---
+++ server/prodKeys.js
@@ -0,0 +1,18 @@
+module.exports = {
+    state: "production",
+    google: {
+        clientID: process.env.clientID_URI,
+        clientSecret: process.env.clientSecret_URI,
+    },
+    facebook: {
+        AppID: process.env.AppID_URI,
+        AppSecret: process.env.AppSecret_URI
+    },
+    mongoDB: {
+        dbURI: process.env.MONGODB_URI
+    },
+    session: {
+        cookieKey: process.env.COOKIE_URI
+    },
+    JWT_secret: process.env.JWT_URI
+}
