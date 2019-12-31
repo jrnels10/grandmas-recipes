14e58d93675d18cb4c844fdcc3afb1bf67f65104
Jacob <jrnels10@gmail.com>
2019-12-30 17:25:50 -0700

    make it better

--- server/cloud/googleCloud.js
+++ server/cloud/googleCloud.js
@@ -1,13 +1,16 @@
+'use strict';
 const { Storage } = require('@google-cloud/storage');
 const keyFilename = './server/config/grandmasRecipes-49091d2bc82f.json';
+const { auth, Compute } = require('google-auth-library');
 const projectId = 'grandmasrecipes';
-const storage = new Storage({ projectId, keyFilename });
-var myBucket = storage.bucket('grandmas-recipes');
 // https://cloud.google.com/storage/docs/reference/libraries#client-libraries-install-nodejs
 // api requests (GET,POST,DELETE,PUT) =>   https://cloud.google.com/storage/docs/json_api/v1/?apix=true
 module.exports = {
     uploadToGoogleCloud: async (req, res, next) => {
         try {
+
+            const storage = new Storage({ projectId, keyFilename });
+            var myBucket = storage.bucket('grandmas-recipes');
             // storage.getBuckets().then(buckets => {
             //     console.log('Buckets:');
             //     buckets.forEach(bucket => {
