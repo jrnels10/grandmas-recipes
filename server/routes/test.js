8df2781da1d82f1563c3f222651fe98aedcca1cf
Jacob <jrnels10@gmail.com>
2019-12-30 18:40:43 -0700

    testconnection

--- server/routes/test.js
+++ server/routes/test.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = require('express-promise-router')();
 const TestController = require('../controllers/testcontroller');
 
-router.route("/testConnection")
+router.route("/testconnection")
     .post(TestController.connection);
 
 module.exports = router;
