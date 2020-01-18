24c409140b151f3c0bb1c2f12c63e97d2fd21f8a
Jacob <jrnels10@gmail.com>
2020-01-17 18:07:00 -0700

    temporarily removed image size restrictions

--- server/routes/users.js
+++ server/routes/users.js
@@ -31,9 +31,9 @@ const storage = multer.diskStorage({
 })
 const upload = multer({
     storage: storage,
-    limits: {
-        fileSize: 1500 * 1500 * 1
-    }
+    // limits: {
+    //     fileSize: 1500 * 1500 * 1
+    // }
 });
 
 
