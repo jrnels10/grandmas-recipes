f948551004d238fac4a7a84708a23326830744e2
Jacob <jrnels10@gmail.com>
2020-01-26 21:42:31 -0700

    reset path to addrecipe

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,7 @@ import SignIn from './components/sign/SignIn';
 import { Provider } from './Context';
 import authGuard from './components/HOC/authGuard';
 import DashBoard from './components/dashboard/Dashboard';
-import AddRecipe from './components/recipe/AddRecipe';
+// import AddRecipe from './components/recipe/AddRecipe';
 import RecipeCard from './components/cards/RecipeCard';
 import CardContainer from './components/cards/CardContainer';
 import AddChef from './components/cards/AddChef';
@@ -27,7 +27,7 @@ ReactDOM.render(
                     <Route path="/dashboard" component={authGuard(DashBoard)} />
                     <Route path="/familychefs" component={authGuard(CardContainer)} />
                     <Route path="/addnewchef" component={authGuard(AddChef)} />
-                    <Route path="/addrecipe/:id" component={authGuard(AddRecipe)} />
+                    {/* <Route path="/addrecipe/:id" component={authGuard(AddRecipe)} /> */}
                     <Route path="/familychef/:id" component={SingleChefCard} />
                     <Route path={`/recipe/selectedrecipe/:recipeId`} component={RecipeCard} />
                     <Route exact path="/signin" component={SignIn} />
