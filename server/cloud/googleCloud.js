99b7d753fa3ddc8b90abc043695091afd84c2550
Jacob <jrnels10@gmail.com>
2020-01-27 00:17:25 -0700

    uploading resized images to google cloud, need callback when file is finished uploading

--- server/cloud/googleCloud.js
+++ server/cloud/googleCloud.js
@@ -1,18 +1,37 @@
 'use strict';
 const { Storage } = require('@google-cloud/storage');
-// const keyFilename = './server/config/grandmasRecipes-49091d2bc82f.json';
-// const keyFilename = process.env.GOOGLE_APPLICATION_CREDENTIALS;
 const keyFilename = process.env.NODE_ENV === "production" ? process.env.GOOGLE_APPLICATION_CREDENTIALS : './server/config/grandmasRecipes-49091d2bc82f.json';
 const projectId = 'grandmasrecipes';
+const fs = require('fs');
+// https://googleapis.dev/nodejs/storage/latest/Bucket.html#upload
+// https://googleapis.dev/nodejs/storage/latest/File.html#createWriteStream
 // https://cloud.google.com/storage/docs/reference/libraries#client-libraries-install-nodejs
 // api requests (GET,POST,DELETE,PUT) =>   https://cloud.google.com/storage/docs/json_api/v1/?apix=true
 module.exports = {
     uploadToGoogleCloud: async (req, res, next) => {
         try {
-            let path = req.path;
+            const gcsname = `_resized_${req.file.originalname}`;
             const storage = new Storage({ projectId, keyFilename });
-            const imageUploaded = await storage.bucket('grandmas-recipes').upload(path, { public: true });
-            return (imageUploaded)
+
+            const bucket = await storage.bucket('grandmas-recipes')
+            const file = bucket.file(gcsname);
+            let status = 'hello'
+            fs.createReadStream(`./server/uploads/_resized_${req.file.originalname}`)
+                .pipe(file.createWriteStream({
+                    metadata: {
+                        contentType: 'image/jpeg',
+                        metadata: {
+                            custom: 'metadata'
+                        }
+                    }
+                }))
+                .on('error', function (err) {
+                    return status = err
+                })
+                .on('finish', function () {
+                    req.file.cloudStorageObject = gcsname;
+                    return status
+                });
         } catch (err) {
             console.error('ERROR:', err);
         };
--- server/controllers/chefs.js
+++ server/controllers/chefs.js
@@ -1,7 +1,9 @@
 const User = require('../models/user');
 const { uploadToGoogleCloud, deleteImageFromGoogleCloud } = require('../cloud/googleCloud');
-var multer = require('multer')
-var upload = multer().single('avatar')
+const sharp = require('sharp');
+const path = require('path')
+const fs = require('fs');
+
 module.exports = {
     findMyChef: async (req, res, next) => {
         try {
@@ -15,37 +17,45 @@ module.exports = {
         }
     },
     addMyChef: async (req, res, next) => {
+        const { filename: filename } = req.file
         try {
             // console.log('test')
             console.log('====================add new chef controller====================')
-
+            console.log(req.file)
+            await sharp(req.file.path)
+                .resize(500)
+                .jpeg({ quality: 50 })
+                .toFile(
+                    path.resolve(req.file.destination, `_resized_${filename}`)
+                )
+            fs.unlinkSync(req.file.path)
             let mine = req.body.myRecipes;
             let picture;
             const accountType = req.body.accountType === "google" ? 'google.email' :
                 req.body.accountType === "facebook" ? 'facebook.email' : 'local.email';
             const foundGrandmaList = await User.findOne({ [accountType]: req.params.email })
             if (req.file !== undefined && accountType !== undefined) {
-                const imageUploaded = await uploadToGoogleCloud(req.file);
-                // console.log(imageUploaded)
-                picture = `https://storage.googleapis.com/${imageUploaded[0].metadata.bucket}/${imageUploaded[0].metadata.name}`;
+                const imageUploaded = await uploadToGoogleCloud({ file: req.file });
+                console.log(imageUploaded)
+                picture = `https://storage.googleapis.com/grandmas-recipes/_resized_${req.file.originalname}`;
                 console.log('====================chef picture uploaded controller====================')
 
             }
 
             let myRecipes = foundGrandmaList.myRecipes;
             myRecipes.push(Object.assign(JSON.parse(mine), { chefImage: picture }));
-            return await User.findOneAndUpdate({ [accountType]: req.params.email }, {
-                "myRecipes": myRecipes
-            }).then((profile) => {
-                User.findOne({ [accountType]: req.params.email }).then(function (item) {
-                    res.send(item)
-                });
-                console.log('====================add new completed controller====================')
+            // return await User.findOneAndUpdate({ [accountType]: req.params.email }, {
+            //     "myRecipes": myRecipes
+            // }).then((profile) => {
+            //     User.findOne({ [accountType]: req.params.email }).then(function (item) {
+            //         res.send(item)
+            //     });
+            //     console.log('====================add new completed controller====================')
 
-            }).catch(error => {
-                res.send(404)
-                return error
-            });
+            // }).catch(error => {
+            //     res.send(404)
+            //     return error
+            // });
         } catch (error) {
             console.log(error)
             res.send({ error: error, errorMessage: 'Cannot add chef, this could be due to invalid email.' })
